d72805b1af9eb089db5f2bf62bbec4534fdbc8da
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2022-10-24 21:07:02 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AsyncDisplayKit",
-            url: "https://github.com/FlowCrypt/Texture/releases/download/0.0.1/AsyncDisplayKit.xcframework.zip",
+            url: "https://github.com/FlowCrypt/Texture/releases/download/3.1.0/AsyncDisplayKit.xcframework.zip",
             checksum: "55a7dbd5c9b78bf7c6e1a38314f316734e6865a08206dd478b1dca59dc85e9ee"),
     ]
 )
